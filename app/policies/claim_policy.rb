e990cf133aaa3bf687cc7922535cc9253ac04b0f
adoniyas7 <adoniyasg7s@gmail.com>
2026-02-04 01:37:33 +0300

    refactor: restructure ClaimPolicy class for improved readability

--- app/policies/claim_policy.rb
+++ app/policies/claim_policy.rb
@@ -1,15 +1,17 @@
-class Scope < ApplicationPolicy::Scope
-  def resolve
-    return scope.all if Rails.env.test?
+class ClaimPolicy < ApplicationPolicy
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      return scope.all if Rails.env.test?
 
-    if user.has_role?("admin")
-      scope.all
-    elsif user.insurer.present?
-      insurer_id = user.insurer.id
-      scope.joins(policy: { coverage_type: { insurance_product: :insurer } })
-            .where(insurers: { id: insurer_id })
-    else
-      scope.joins(:policy).where(policies: { user_id: user.id })
+      if user.has_role?("admin")
+        scope.all
+      elsif user.insurer.present?
+        insurer_id = user.insurer.id
+        scope.joins(policy: { coverage_type: { insurance_product: :insurer } })
+              .where(insurers: { id: insurer_id })
+      else
+        scope.joins(:policy).where(policies: { user_id: user.id })
+      end
     end
   end
 end
